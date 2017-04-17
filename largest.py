471578c93f70ac96c77920a0f58ad2352115def1
pkksrct <aprakashraj07@gmail.com>
2017-04-17 19:23:46 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,6 +1,6 @@
-num1=56.23
-num2=53.56
-num3=45.54
+num1=int(input("enter the 1st number"))
+num2=int(input("enter the 2nd number"))
+num3=int(input("enter the 3rd number"))
 if (num1>num2) and (num1>num3):
     largest=num1
 elif(num2>num1) and (num2>num3):
