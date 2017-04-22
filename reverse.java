4190250c2f4539d5c09cced9bc86e774a724ec5f
pkksrct <aprakashraj07@gmail.com>
2017-04-22 18:37:54 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class rev
+{
+   public static void main(String[] args) throws IOException
+    {
+  String original, reverse = "";
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter a string to reverse");
+      original = in.nextLine();
+ 
+      int length = original.length();
+ 
+      for ( int i = length - 1 ; i >= 0 ; i-- )
+         reverse = reverse + original.charAt(i);
+ 
+      System.out.println("Reverse of entered string is: "+reverse);
+    
+    }
+
+}
+
