cc96df6bd9edead6c38d682441eea358a19cae0e
pkksrct <aprakashraj07@gmail.com>
2017-06-07 18:52:00 +0530

    Create strti.java

---
+++ strti.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class strti
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner(System.in);
+        String s=n.nextLine();
+        int f=Integer.parseInt(s);
+        System.out.println(f);
+    }
+}
+
